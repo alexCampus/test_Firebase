a6c7b383458d8e5d42a8b77fe37236031978a954
Alex De Pembroke <alexandre.depembroke@campus-numerique-in-the-alps.com>
2017-10-06 07:56:08 +0200

    add loader and winner

--- back/script/firebase.js
+++ back/script/firebase.js
@@ -1,30 +1,54 @@
 // Initialize Firebase
   var config = {
-   
+  	apiKey: "AIzaSyAlJ1vxffN49vHV2N87n4GRha9tjIzHNds",
+    authDomain: "humantalks-firebase.firebaseapp.com",
+    databaseURL: "https://humantalks-firebase.firebaseio.com",
+    projectId: "humantalks-firebase",
+    storageBucket: "humantalks-firebase.appspot.com",
+    messagingSenderId: "368564411937"
   };
   firebase.initializeApp(config);
 
 let current_progress = 0;
-var userCountRef = firebase.database().ref();
+let dbRef            = firebase.database().ref();
+let countWinnerPlace = dbRef.child('countWinnerPlace/');
 
-userCountRef.on('value', function(snapshot) {
+dbRef.on('value', function(snapshot) {
 	$('.game').empty();
   	console.log(snapshot.val());
   	$.each(snapshot.val(), function(index, value){
-  		$('.game').append('<div class="progress align-self-center" id="' + value.username + '">');
-  		$('#' + value.username).append('<h1>' + value.username + '</h1>');
-  		$('#' + value.username).append('<div id="dynamic_'+ value.username +'" class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%">');
-		$('#dynamic_' + value.username).append('<span id="current-progress_"' + value.username + '></span>'); 	  	
-  		console.log(value.count);
-	  	current_progress = value.count;
-	  	$("#dynamic_" + value.username)
-	      	.css("width", current_progress + "%")
-	      	.attr("aria-valuenow", current_progress)
-	      	.text(current_progress + "% Complete");
+  		if (value.count != undefined) {
+	  		$('.game').append('<div class="progress align-self-center" id="' + value.username + '">');
+	  		$('#' + value.username).before('<h1>' + value.username + '</h1>');
+	  		$('#' + value.username).append('<div id="dynamic_'+ value.username +'" class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%">');
+			$('#dynamic_' + value.username).append('<span id="current-progress_"' + value.username + '></span>'); 	  	
+	  		console.log(value.count);
+		  	current_progress = value.count;
+		  	$("#dynamic_" + value.username)
+		      	.css("width", current_progress + "%")
+		      	.attr("aria-valuenow", current_progress)
+		      	.text(current_progress + "% Complete");
 
-	    	if (current_progress >= 100) {
-	    		$("#dynamic_" + value.username).addClass('hidden');
-	    	}
+		    	if (current_progress >= 100) {
+		    		$("#dynamic_" + value.username).addClass('hidden');
+		    		$('#' + value.username).after('<h2>' + value.place + '</h2>');
+		    	}
+  			
+  		}
   	});
 
 });
+
+	function startGame() {
+	  dbRef.child('startGame/').set(true);
+	  dbRef.child('countWinnerPlace/').set(1);
+	  $('#startGame').addClass('hidden');
+	  $('#endGame').removeClass('hidden');
+	}
+
+	function endGame() {
+	  dbRef.remove();
+	  dbRef.child('startGame/').set(false);
+	  $('#endGame').addClass('hidden');
+	  $('#startGame').removeClass('hidden');
+	}
--- client/script/firebase.js
+++ client/script/firebase.js
@@ -1,23 +1,67 @@
 // Initialize Firebase
 var config = {
-
+	apiKey: "AIzaSyAlJ1vxffN49vHV2N87n4GRha9tjIzHNds",
+    authDomain: "humantalks-firebase.firebaseapp.com",
+    databaseURL: "https://humantalks-firebase.firebaseio.com",
+    projectId: "humantalks-firebase",
+    storageBucket: "humantalks-firebase.appspot.com",
+    messagingSenderId: "368564411937"
 };
 
 firebase.initializeApp(config);
-
-let count = 0;
+let dbRef            = firebase.database().ref();
+let countWinnerPlace = dbRef.child('countWinnerPlace/');
+let count            = 0;
 let name;
+let user;
 
 function getName() {
 	name  = $('#name').val();
 	console.log(name);
 	$('.input-group').addClass('hidden');
-	$('#game').removeClass('hidden');
-}
-function writeUserData(userId) {
-	count++;
-	firebase.database().ref('/' + name).set({
+	$('#loader').removeClass('hidden');
+	user = dbRef.child('/' + name);
+	user.set({
 		username: name,
-		count   : count
+		count   : 0,
+		place   : 0
 	});
 }
+function writeUserData() {
+	count ++;
+	user.update({
+		count: count
+	});
+	user.once('value').then(function(snapshot) {
+		
+		count = snapshot.val().count;
+		if (count >= 100) {
+			countWinnerPlace.once('value').then(function(snap){
+				console.log(snap.val());
+				$('#loader').after('<h1>' + snap.val() + '</h1>');
+				user.update({
+					place: snap.val()
+				});
+				dbRef.child('countWinnerPlace/').set(snap.val()+1);
+
+			});
+			$('#game').addClass('hiddenPlus');
+			// $('#loader').removeClass('hidden');
+		}
+	})
+}
+
+var startGame = dbRef.child('/startGame');
+
+startGame.on('value', function(snapshot) {
+	if (snapshot.val() === true) {
+		$('#loader').addClass('hidden');
+		$('#game').removeClass('hiddenPlus');
+	} 
+	if (snapshot.val() === false) {
+		$('#game').addClass('hiddenPlus');
+		$('.input-group').removeClass('hidden');
+		
+	}
+	console.log(snapshot.val());
+});
